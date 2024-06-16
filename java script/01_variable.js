02fee9de28573e4b72e069305d14fb6ede90e408
Daksh2256 <dakshk5610@gmail.com>
2024-06-16 19:31:47 +0530

    Add files via upload

---
+++ java script/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 989564
+let accountEmail = "Daksh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Junagadh"
+let accountState;
+// accountId = 2 // not allowed for js
+
+console.log(accountId);
+
+accountEmail = "DK@gmail.com"
+accountPassword = "5610"
+accountCity = "kota"
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
+
+// not use var tag beacuse isusee in block scope and functional scope 
---
+++ java script/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; //use  a js use be a newer version
+
+// alert(3 + 3) // we are using node js, not broweser node js ma error always
+
+console.log(3 + 3),
+
+console.log("Daksh")
+
+let name = "Daksh"
+let age = 18
+let isLoggendIn = false
+
+//number => 2 to power 53
+// string => ""
+// boolean => true/false
+// bigint
+// null => standalone value
+// symbol => unique
+// object 
+
+console.log(typeof age);
+ 
